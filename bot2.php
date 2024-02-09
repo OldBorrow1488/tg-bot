2fcda1a4ddc4dd53b14fa1fb02675edcf1bcb7b7
Kirill <113068101+OldBorrow1488@users.noreply.github.com>
2024-02-09 14:20:37 +0300

    Обработка инлайн кнопок

--- bot2.php
+++ bot2.php
@@ -13,6 +13,8 @@ use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 use danog\MadelineProto\EventHandler\SimpleFilter\Incoming;
 use danog\MadelineProto\EventHandler\Keyboard\InlineKeyboard;
+use danog\MadelineProto\EventHandler\CallbackQuery;
+use danog\MadelineProto\MTProto;
 
 // Путь к файлу сессии MadelineProto
 $sessionPath = 'session.madeline';
@@ -53,7 +55,7 @@ class BasicEventHandler extends SimpleEventHandler
             // Получение текста сообщения
             $messageText = $message->message;
             // Проверка на наличие приветственных слов в сообщении
-            if (stripos($messageText, 'привет') !== false || stripos($messageText, 'здравствуй') !== false) {
+            if (stripos($messageText, 'Привет') !== false || stripos($messageText, 'Здравствуй') !== false) {
                 // Отправка ответного приветствия
                 $this->messages->sendMessage(peer: $message->senderId, message: 'Здравствуйте, уважаемый пользователь!');
                 // Запись в лог информации об отправленном сообщении
@@ -67,27 +69,9 @@ class BasicEventHandler extends SimpleEventHandler
 
     // Обработчик команды /start
     #[FilterCommand('start')]
-    public function startCommand(Message $message): void
-    {
-        try {
-            $keyboard = [
-                'inline_keyboard' => [
-                    [
-                        ['text' => 'Посетить GitHub', 'url' => 'https://github.com'],
-                    ],
-                    [
-                        ['text' => 'Посетить Telegram', 'url' => 'https://t.me'],
-                    ]
-                ]
-            ];
-            $this->messages->sendMessage(
-                peer:  $message->senderId,
-                message: 'Привет! Бот активен.',
-                reply_markup: $keyboard,);
-            $this->getCustomLogger()->info("Стартовое сообщение отправлено пользователю: {$message->senderId}");
-        } catch (\Exception $e) {
-            $this->getCustomLogger()->error("Ошибка при отправке стартового сообщения: " . $e->getMessage());
-        }
+    public function startCommand(Message $message): void {
+        $this->sendMenu($message->senderId);
+        $this->getCustomLogger()->info("Меню отправлено пользователю: {$message->senderId}");
     }
 
     // Обработчик команды /help
@@ -110,6 +94,111 @@ class BasicEventHandler extends SimpleEventHandler
         } catch (\Exception $e) {
             $this->getCustomLogger()->error("Ошибка при отправке стартового сообщения: " . $e->getMessage());
         }
+    }
+
+    // Обработчик команды, отправляющий обычную клавиатуру
+    #[FilterCommand('keyboard')]
+    public function sendReplyKeyboard(Message $message): void
+    {
+        // Создание и отправка обычной клавиатуры
+        $replyKeyboard = [
+            'resize_keyboard' => true,
+            'one_time_keyboard' => true,
+            'keyboard' => [
+                [['text' => 'Привет']],
+                [['text' => 'Здравствуй']],
+                [['text' => '/start']],
+            ],
+        ];
+        $this->messages->sendMessage(
+            peer: $message->senderId,
+            message: 'Пожалуйста, выберите один из вариантов:',
+            reply_markup: $replyKeyboard,
+        );
+    }
+
+    public function onAny($update): void
+    {
+        // Проверка на колбэк-запрос
+        if (isset($update['_']) && $update['_'] === 'updateBotCallbackQuery') {
+
+            $this->onCallbackQuery($update);
+        }
+    }
+
+
+
+    // Обработчик колбэк-квэри (нажатий на инлайн-кнопки)
+    public function onCallbackQuery($update) {
+
+        $this->getCustomLogger()->info("Получен колбэк-запрос: " . json_encode($update));
+
+        $callbackData = $update['data'];
+        $chatID = $update['peer'];
+        $messageID = $update['msg_id'];
+        $this->getCustomLogger()->info("Получены данные: " . $callbackData);
+        switch ($callbackData) {
+            case 'about':
+                $this->messages->editMessage(
+                    no_webpage: true,
+                    peer: $chatID,
+                    id: $messageID,
+                    message: 'Информация о боте: [Описание бота]',
+                );
+                break;
+            case 'help':
+                $this->messages->editMessage(
+                    no_webpage: true,
+                    peer: $chatID,
+                    id: $messageID,
+                    message:  'Как использовать бота: [Инструкция]',
+                );
+                break;
+            case 'links':
+                $this->messages->editMessage(
+                    no_webpage: true,
+                    peer: $chatID,
+                    id: $messageID,
+                    message: 'Полезные ссылки: [Ссылки]',
+                );
+                break;
+            default:
+                $this->messages->sendMessage(
+                    no_webpage: true,
+                    peer: $chatID,
+                    message: 'Неизвестная команда.',
+                );
+                break;
+        }
+
+        // Отправка подтверждения обработки колбэк-запроса
+        try {
+            $this->messages->getBotCallbackAnswer(
+                game: false,
+                peer: $chatID,
+                msg_id: $messageID,
+                data: $callbackData,
+            );
+        } catch (\Exception $e) {
+            $this->getCustomLogger()->error("Ошибка при отправке ответа на колбэк-запрос: " . $e->getMessage());
+        }
+    }
+
+    // Метод для отправки меню с инлайн-кнопками
+    public function sendMenu($chatID) {
+        $keyboard = [
+            'inline_keyboard' => [
+                [['text' => 'О боте', 'callback_data' => 'about']],
+                [['text' => 'Помощь', 'callback_data' => 'help']],
+                [['text' => 'Ссылки', 'callback_data' => 'links']],
+            ],
+        ];
+
+        $this->messages->sendMessage(
+            peer: $chatID,
+            message:  'Выберите опцию из меню:',
+            reply_markup: $keyboard,
+        );
     }
 
 
